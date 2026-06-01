5d0cbc46c7dd91fdd0711322aaa1a6f3940ec7c5
iconfident1314 <chongli920178@gmail.com>
2026-06-01 22:35:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活
 const FILE_PATH = process.env.FILE_PATH || '.tmp';   // 运行目录,sub节点文件保存目录
 const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '037068be-ecd1-471d-b565-56fdf507a35f'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const UUID = process.env.UUID || '12c8e1b7-3d92-4e82-a021-0aae1a6d3486'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'laowang11.iconfident.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiY2U0MGNkNWQ5Nzg0YTExOTcwYmVkZWQzYmQ5OGIxOWMiLCJ0IjoiZmM4MWYyN2UtMWEzOS00ODcyLWFkNGEtYjI0YzNkZjU4ODFkIiwicyI6IlpEUmlZall4T1RRdE1tWmtOUzAwTVRGbExUaGtORFV0T1RVeVlXVTJPV0ZoWVdNNSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'dcdeploy.170305.ccwu.cc';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGM2Y2I1M2QxMTZkNDM4Njg4YWUwMjQ5YjJlNjlhZGUiLCJ0IjoiNGU2NWJlNjUtMTYyYS00Y2Y4LTkyYjctNGExOWVjOWE3NTk0IiwicyI6Ill6UTBaR001WldRdFkySTVaaTAwTkRFMUxUZ3laRGN0TkdNeFlUVTRORFptTVdVeCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
@@ -150,7 +150,7 @@ function downloadFile(fileName, fileUrl, callback) {
     url: fileUrl,
     responseType: 'stream',
   })
-    .then(response => {
+    。键，然后(response => {
       response.data.pipe(writer);
 
       writer.on('finish', () => {
